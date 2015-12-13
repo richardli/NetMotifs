9d543cdd900becd09c95e96d63c0878e61aecbc3
richardli <lizehang@gmail.com>
2015-12-13 11:20:22 -0800

    add HashMap wrapper to bypass subclass

--- src/data/NodeMotifHashMap.java
+++ src/data/NodeMotifHashMap.java
@@ -1,7 +1,10 @@
 package data;
 
+import java.util.HashMap;
+
 /**
  * Created by zehangli on 12/13/15.
  */
 public class NodeMotifHashMap {
+    public HashMap<Integer, NodeMotifwithNeighbour> nodes;
 }
