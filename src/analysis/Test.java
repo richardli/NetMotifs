695c3db3e793de6cafe5c8c8bb07dc5d3fd10599
richardli <lizehang@gmail.com>
2016-06-03 10:35:41 -0700

    minor fixes

--- src/analysis/Test.java
+++ src/analysis/Test.java
@@ -1,14 +1,33 @@
 package analysis;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by zehangli on 7/13/15.
  */
 public class Test {
+
+    Set<Integer> test = new HashSet<Integer>();
+
+
     public static void main(String[] args) {
         // psvm -> main
         // sout -> sysout
         // souf -> sysout printf
-        System.out.println("Test, world");
+        Test a0 = new Test();
+        a0.test.add(1);
+        a0.test.add(null);
+        System.out.print(a0.test.size());
+
+        double[][] test = new double[2][3];
+        for (double[] row: test) Arrays.fill(row, 123.0);
+        System.out.println("first");
+        System.out.println(Arrays.toString(test[0]));
+        System.out.println("second");
+        System.out.println(Arrays.toString(test[1]));
+        System.out.println(test[0][1]);
 
     }
 }
--- src/util/MathUtil.java
+++ src/util/MathUtil.java
@@ -6,6 +6,27 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class MathUtil {
+
+    /**
+     * For (i, j, k) entry in D1 x D2 x D3 array,
+     *    return order (starting from 0) of the entry
+     * e.g., (0, 2, 1) entry in 5x6x7 array:
+     *        order is 0 * 6 * 7 + 2 * 7 + 1 = 15
+     * **/
+    public static int orderhash(int D2, int D3, int i, int j ,int k){
+        return(i * D2 * D3 + j * D3 + k);
+    }
+
+    public static double logSumOfExponentials(double x1, double x2) {
+        double max = x1;
+        if(x2 > x1) max = x2;
+        double sum = 0.0;
+        if (x1 != Double.NEGATIVE_INFINITY)
+            sum += java.lang.Math.exp(x1 - max);
+        if (x2 != Double.NEGATIVE_INFINITY)
+            sum += java.lang.Math.exp(x2 - max);
+        return max + java.lang.Math.log(sum);
+    }
     /**
      * Log Gamma function
      *
